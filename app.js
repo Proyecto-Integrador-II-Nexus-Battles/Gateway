2345368e182c7b0add47227be971d82ce41e7321
David Cárdenas <santito500@live.com>
2024-03-03 12:08:35 -0500

    refactor: :art: Add dev script and dependencies, update app.js
    Add dev script and dependencies, update app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-import express, { json } from 'express'
-import { templateRouter } from './routes/template.js' //--> !!!IMPORTANT!!! Siempre que importen un archivo extensión .js .Loquesea, siempre ponerlo en el path, ej -> './routes/template.js' --> el .js es la extensión 
+import express, { json } from "express";
+import { PORT } from "./config.js";
+import expressGateway from "express-gateway";
 
-const app = express() // --> Iniciamos express
-app.use(json()) 
-app.disable('x-powered-by') // --> Deshabilitar el header x-powered-by
-
-app.use(templateRouter)
-
-const PORT = process.env.PORT || 3000 // --> Usar la variable de entorno PORT, si no usar el port 3000
+const app = express();
+app.use(json());
+expressGateway(app);
 
 app.listen(PORT, () => {
-  console.log(`Server listen on port http://localhost:${PORT}`)
-})
+  console.log(`Server listen on port ${PORT}`);
+});
